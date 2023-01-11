021026d41e0dd831417d1ef0cd0b72133915ebbc
madhusudhan1304 <113178127+madhusudhan1304@users.noreply.github.com>
2023-01-11 19:24:32 +0530

    Update 20221120052016-create-user.js

--- migrations/20221120052016-create-user.js
+++ migrations/20221120052016-create-user.js
@@ -1,5 +1,4 @@
 "use strict";
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Users", {
@@ -11,7 +10,6 @@ module.exports = {
       },
       firstName: {
         type: Sequelize.STRING,
-        allowNull: false,
       },
       lastName: {
         type: Sequelize.STRING,
